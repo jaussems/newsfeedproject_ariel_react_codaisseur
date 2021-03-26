704fbbd861e757267aa1e6d17142f3c6b205c402
jaussems <jannesaussems@gmail.com>
2021-03-26 16:10:32 +0100

    added the news articles to the homepage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import "./App.css";
-<<<<<<< HEAD
-import NewsArticle from "./components/newsArticleAPI";
-=======
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
->>>>>>> 4bc27b8cc41cdd7922e6f999272388a74b6c7ff0
 function App() {
   return (
     <div className="App">
       <NavBar />
       <h1>Hello world!</h1>
-<<<<<<< HEAD
-      <NewsArticle />
-=======
       <Switch>
         <Route path="/about" component={AboutPage} />
         <Route path="/" component={HomePage} />
       </Switch>
       <Footer />
->>>>>>> 4bc27b8cc41cdd7922e6f999272388a74b6c7ff0
     </div>
   );
 }
